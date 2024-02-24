e3230744d950fd6cce062fb66327915ab57ea374
Oleg Novoselskyy <tatteddev@gmail.com>
2024-02-24 16:06:15 -0500

    refactor

--- TipCalculator/TipCalculator/TipCalculator.py
+++ TipCalculator/TipCalculator/TipCalculator.py
@@ -1,11 +1,15 @@
 print("Welcome to the tip calculator\n")
 
-total = input("What was the total spent\n")
-percent = input("What percent would you like to tip?\n")
-people = input("How many people are splitting the check?\n")
+bill = int(input("What was the total spent\n"))
+tip = int(input("What percent would you like to tip?\n"))
+people = int(input("How many people are splitting the check?\n"))
 
-percent = float(percent) / 100
-tip = (int(total) / int(people)) * percent
+tip_as_percent = tip / 100
+total_tip_amount = bill * tip_as_percent
+total_bill = bill + total_tip_amount
+bill_per_person = total_bill / people
+final_amount = round(bill_per_person, 2)
+final_amount = "{:.2f}".format(bill_per_person)
 
-print(f"The tip would be ${round(tip, 2)} per person")
+print(f"Every person will pay: ${final_amount}")
 
